01f6c33d41f6d62942459a673c7c10d8d306997a
Jannatul Choudhury <jchoudhury18@gmail.com>
2016-02-20 02:00:48 +0000

    first attempt of sipp specification of cars

--- src/main/java/RentalCars.java
+++ src/main/java/RentalCars.java
@@ -13,7 +13,7 @@ import java.util.List;
  * Created by jannatul on 19/02/16.
  */
 public class RentalCars {
-    public static void main(String[] args)  throws Exception{
+    public static void main(String[] args)  throws Exception {
 //        ObjectMapper mapper = new ObjectMapper();
 //        Search obj = mapper.readValue(new URL("http://www.rentalcars.com/js/vehicles.json"), Search.class);
 //        System.out.println(obj);
@@ -23,7 +23,10 @@ public class RentalCars {
         JsonObject jobject = jelement.getAsJsonObject();
         jobject = jobject.getAsJsonObject("Search");
         JsonArray jarray = jobject.getAsJsonArray("VehicleList");
+        System.out.println("A list of all cars in ascending price order");
         listPrice(jarray);
+        System.out.println("\nSIPP specification of all cars");
+        sippSpecification(jarray);
     }
 
     private static List<VehicleList> getCarList(JsonArray jsonArray)
@@ -35,6 +38,7 @@ public class RentalCars {
             VehicleList vehicle = gson.fromJson(jobject.toString(), VehicleList.class);
             cars.add(vehicle);
         }
+
         return cars;
     }
 
@@ -56,10 +60,87 @@ public class RentalCars {
 
         });
 
-        for (VehicleList car: cars) {
-            System.out.println(car.getName() + " - " + car.getPrice());
+        for (int i = 0; i< cars.size(); i++) {
+            System.out.println(i+1 + ". " + cars.get(i).getName() + " - " + cars.get(i).getPrice());
         }
+    }
 
+    public static void sippSpecification(JsonArray carList) {
+        List<VehicleList> cars = getCarList(carList);
+
+        for (int i = 0; i< cars.size(); i++) {
+            char firstLetter = cars.get(i).getSipp().charAt(0);
+            char secondLetter = cars.get(i).getSipp().charAt(1);
+            char thirdLetter = cars.get(i).getSipp().charAt(2);
+            char fourthLetter = cars.get(i).getSipp().charAt(3);
+            String carType, doorsAndType, transmission, aircon = "";
+            String fuel = "Petrol";
+            if (firstLetter == 'M') {
+                carType = "Mini";
+            } else if (firstLetter == 'E') {
+                carType = "Economy";
+            } else if (firstLetter == 'C') {
+                carType = "Compact";
+            } else if (firstLetter == 'I') {
+                carType = "Intermediate";
+            } else if (firstLetter == 'S') {
+                carType = "Standard";
+            } else if (firstLetter == 'F') {
+                carType = "Full size";
+            } else if (firstLetter == 'P') {
+                carType = "Premium";
+            } else if (firstLetter == 'L') {
+                carType = "Luxury";
+            } else if (firstLetter == 'X') {
+                carType = "Special";
+            } else {
+                carType = "undefined car type";
+            }
+
+            if (secondLetter == 'B') {
+                doorsAndType = "2 doors";
+            } else if (secondLetter == 'C') {
+                doorsAndType = "4 doors";
+            } else if (secondLetter == 'D') {
+                doorsAndType = "5 doors";
+            } else if (secondLetter == 'W') {
+                doorsAndType = "Estate";
+            } else if (secondLetter == 'T') {
+                doorsAndType = "Convertible";
+            } else if (secondLetter == 'F') {
+                doorsAndType = "SUV";
+            } else if (secondLetter == 'P') {
+                doorsAndType = "Pick up";
+            } else if (secondLetter == 'V') {
+                doorsAndType = "Passenger Van";
+            } else {
+                if (secondLetter == 'X') {
+                    doorsAndType = "Special";
+                } else {
+                    doorsAndType  = "undefined doors/car type";
+                }
+            }
+
+            if (thirdLetter == 'M') {
+                transmission = "Manual";
+            } else if (thirdLetter == 'A') {
+                transmission = "Automatic";
+            } else {
+                transmission = "undefined transmission";
+            }
+
+            if (fourthLetter == 'N') {
+                aircon = "no air conditioning";
+            } else if (fourthLetter == 'R') {
+                aircon = "air conditioning";
+            } else {
+                aircon = "aircon not defined";
+            }
+
+            System.out.println(i + 1 + ". " + cars.get(i).getName() + " - " + cars.get(i).getSipp()
+                    + " - " + carType + " - " + doorsAndType + " - "
+                    + transmission + " - " + fuel + " - " + aircon);
+        }
     }
 
     private static String readUrl(String urlString) throws Exception {
